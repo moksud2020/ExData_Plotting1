1f69462d33be82cdc959ae0bfd3cbe676a237e1c
moksud2020 <moksud@metrouni.edu.bd>
2020-06-23 22:21:09 +0600

    Update plot1

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ powerData <- read.csv("household_power_consumption.txt", header=T, sep=';', na.s
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 dataSet <- subset(powerData, Date %in% c("1/2/2007","2/2/2007"))
 
-data$Date <- as.Date(dataSet$Date, format="%d/%m/%Y")
+dataSet$Date <- as.Date(dataSet$Date, format="%d/%m/%Y")
 
 hist(dataSet$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
